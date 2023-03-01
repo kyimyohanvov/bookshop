f17b74982a737fd4b5eb74bd62e29c6137f4f2f1
kmhvov <kyimyohanvov@gmail.com>
2023-03-01 12:13:27 +0800

    Show Books at Home Page

--- BookShop/Areas/Customer/Controllers/HomeController.cs
+++ BookShop/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BookShop.Models;
+using BookShop.Repositories.IRepository;
 
 namespace BookShop.Controllers;
 
@@ -8,15 +9,18 @@ namespace BookShop.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _db;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _db = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Book> bookList = _db.Book.GetAll(includeProperties: "Category");
+        return View(bookList);
     }
 
     public IActionResult Privacy()
